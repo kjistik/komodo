d192b3c3cf9243389ba30d842fe613ec79c7418a
kjistik <coronelmartinernesto@gmail.com>
2023-04-28 20:46:42 -0300

    incomplete frontend - home page added

---
+++ komodo-front/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div clas="content">
+    <MainView />
+    <div class="view">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<style>
+body {
+  margin: 0px;
+  padding: 0px;
+  background-color: #333333;
+}
+
+
+.view {
+  min-height: 70%;
+  width: 100%;
+  margin-top: 10px;
+}
+</style>
+<script>
+import NavBar from './components/NavBar.vue';
+import MainView from './views/MainView.vue'
+export default {
+  components: {
+    NavBar,
+    MainView
+  }
+
+}
+</script>
